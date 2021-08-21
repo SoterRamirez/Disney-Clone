4fb3287efe3839935c56af845c7a75d0a40ea97f
Soter Ramirez <soterrc@gmail.com>
2021-08-21 00:09:28 -0500

    sliders

--- data.js
+++ data.js
@@ -2,26 +2,26 @@ let movies = [
   {
     name: "falcon and the winter soldier",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "Images/slider 2.png",
+    image: "Images/slider2.png",
   },
   {
     name: "loki",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "Images/slider 1.png",
+    image: "Images/slider1.png",
   },
   {
     name: "wanda vision",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "Images/slider 3.png",
+    image: "Images/slider3.png",
   },
   {
     name: "raya and the last dragon",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "Images/slider 4.png",
+    image: "Images/slider4.png",
   },
   {
     name: "luca",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "Images/slider 5.png",
+    image: "Images/slider5.png",
   },
 ];
