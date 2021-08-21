7d7cc74e39b45912a7eebd1198b484e537920939
Soter Ramirez <soterrc@gmail.com>
2021-08-21 00:08:14 -0500

    Se cambia nombre de archivos

--- data.js
+++ data.js
@@ -7,21 +7,21 @@ let movies = [
   {
     name: "loki",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "Images/slider 1.PNG",
+    image: "Images/slider 1.png",
   },
   {
     name: "wanda vision",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "Images/slider 3.PNG",
+    image: "Images/slider 3.png",
   },
   {
     name: "raya and the last dragon",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "Images/slider 4.PNG",
+    image: "Images/slider 4.png",
   },
   {
     name: "luca",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "Images/slider 5.PNG",
+    image: "Images/slider 5.png",
   },
 ];
