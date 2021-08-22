1e4f17231acdfeda85ac0af4f3a6186ba246bd4e
Soter Ramirez <soterrc@gmail.com>
2021-08-21 19:56:01 -0500

    Updade Images to images

--- data.js
+++ data.js
@@ -2,26 +2,26 @@ let movies = [
   {
     name: "falcon and the winter soldier",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "Images/slider2.png",
+    image: "images/slider2.png",
   },
   {
     name: "loki",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "Images/slider1.png",
+    image: "images/slider1.png",
   },
   {
     name: "wanda vision",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "Images/slider3.png",
+    image: "images/slider3.png",
   },
   {
     name: "raya and the last dragon",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "Images/slider4.png",
+    image: "images/slider4.png",
   },
   {
     name: "luca",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "Images/slider5.png",
+    image: "images/slider5.png",
   },
 ];
